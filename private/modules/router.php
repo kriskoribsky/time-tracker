40a0b33dd534e34140d641f22f3dd8d1cb47acce
kriskoribsky <kristian.koribsky@gmail.com>
2022-08-05 23:15:31 +0200

    add front controller router

--- private/modules/router.php
+++ private/modules/router.php
@@ -3,18 +3,39 @@
 
 class Router {
 
-    private static array $allowed_requests = [
+    private static array $allowed_pages = [
         'dashboard',
         'session',
-        'task'
+        'task',
+        '404'
     ];
 
     public function __construct() {}
 
-    // public function 
+    private function parse_uri(string $uri): array {
+        return parse_url($uri);
+    }
 
+    public function process_uri(string $uri, array $get, array $post):void {
+        $uri_parts = $this->parse_uri($uri);
+        $path = $uri_parts['path'];
 
-    public function process_uri(string $uri):void {
-        echo $uri;
+        switch($path) {
+            case '/':
+            case '/dashboard':
+                require_once Helper\Path::build_path(PUBLIC_PATH, 'view', 'pages', 'dashboard.php');
+                break;
+            case '/sessions':
+                require_once Helper\Path::build_path(PUBLIC_PATH, 'view', 'pages', 'sessions.php');
+                break;
+            case '/tasks':
+                require_once Helper\Path::build_path(PUBLIC_PATH, 'view', 'pages', 'tasks.php');
+                break;
+            case '/forms':
+                require_once Helper\Path::build_path(PRIVATE_PATH, 'controller', 'forms.php');
+                break;
+            default:
+                require_once Helper\Path::build_path(PUBLIC_PATH, 'view', 'pages', 'errors', '404.php');
+        }
     }
 }
