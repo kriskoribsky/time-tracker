a345790d75958eb93e20b9b4675497ad26a10e9e
kriskoribsky <kristian.koribsky@gmail.com>
2022-08-05 12:23:14 +0200

    404 page styles

--- public/view/pages/errors/404.php
+++ public/view/pages/errors/404.php
@@ -4,9 +4,12 @@
 
     <div class="http-error text-center">
 
-        <h1 class="http-error-caption text-center">404</h1>
+        <h1 class="text-center">404</h1>
 
-        <h4 class="http-error-msg text-center">Page not found</h4>
+        <h4 class="text-center">Page not found</h4>
+
+        <p class="text-center">I looks like the page you are looking for doesn't exist.</p>
+        <p><a href="/dashboard">← Back to dashboard</a></p>
 
     </div>
 
