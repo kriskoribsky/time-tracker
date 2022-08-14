6ed00b6cf7fbc42717f31324204fb4e09cbd9625
kriskoribsky <kristian.koribsky@gmail.com>
2022-08-14 13:45:21 +0200

    bezier control points graph line render in progress

--- public/view/js/graph.js
+++ public/view/js/graph.js
@@ -105,114 +105,44 @@
     function ceilPixel(pixels) {
         return Math.ceil(pixels);
     } 
-    // rendering of y axis legend
-    // function renderY(ctx, width, height, p, g) {
-    //     ctx.fillStyle = legendColor;
-    //     ctx.strokeStyle = legendColor;
-    //     ctx.lineWidth = 0.5;
-    //     ctx.setLineDash([3 * window.devicePixelRatio, 3 * window.devicePixelRatio]);
-    //     ctx.textBaseline = "middle";
-    //     ctx.textAlign = "end";
 
-    //     const n = yUnits.length;
-    //     const startY = p;
+    function triangleCentroid(x1, x2, y1 ,y2, z1, z2) {
+        // centroid of a triagle is anarithmetic mean of its x and y coordinates
+        tx = (x1 + y1 + z1) / 3;
+        ty = (x2 + y2 + z2) / 3;
 
-    //     // measure required horizontal space
-    //     const textWidth = ceilPixel(ctx.measureText(longestYunit).width);
-    //     const yIncrement = floorPixel((height - 2 * p) / (n-1));
+        return [tx, ty];
+    }
 
-    //     let y = startY;
-    //     for (let i=0; i < n; i++) {
-    //         // y axis legend text
-    //         ctx.fillText(yUnits[i], textWidth, height - y);
+    function cubicBezier(x1, y1, x2, y2) {
+        // cubic bezier control points for graph will be
+        // centroids of their right triangles from midpoint
 
-    //         // dashed line with aplha value
-    //         ctx.globalAlpha = 0.7;
+        // midpoint coords
+        sx = Math.abs(x2 + x1) / 2;
+        sy = Math.abs(y2 + y1) / 2;
 
-    //         ctx.beginPath();
-    //         // little shift (g/2) to the right for the xAxis legend
-    //         ctx.moveTo(textWidth + g - g/2, height - y);
-    //         ctx.lineTo(width - p + g/2, height - y);
-    //         ctx.stroke();
-    //         ctx.globalAlpha = 1;
+        // control point 1 (the last 2 coordinates form the foot of the right triangle)
+        const [cp1x, cp1y] = triangleCentroid(x1, y1, sx, sy, sx, y1);
+        const [cp2x, cp2y] = triangleCentroid(x2, y2, sx, sy, sx, y2);
 
-    //         y += yIncrement;
-    //     }
 
-    //     endY = y - yIncrement;
-
-    //     return [textWidth + g, startY, endY];
-    // }
-
-    function calculateCubicBezier(x1, y1, x2, y2) {
-        // calculate midpoint
-        sX = (x2 - x1) / 2; // always positive as x1 is before x2 on graph
-        sY = (y2 - y1) / 2;   // can be negative if y2 is higher than y1 
-
-        // ratios determining shape of cubic beziér
-        const xRatio = 2/3;
-        const yRatio = 1/3;
-
-        // control points
-        cp1x = x1 + xRatio * sX;
-        cp1y = y1 + yRatio * sY;
-
-        cp2x = x2 - xRatio * sX;
-        cp2y = y2 - xRatio * sY;
-
-        // return canva's cubic bezier arguments format
         return [cp1x, cp1y, cp2x, cp2y, x2, y2];
     }
 
-    // function renderX(ctx, startX, startY, endY, width, height, radius, p, g) {
-    //     ctx.textBaseline = "ideographic";
-    //     ctx.textAlign = "center";
-    //     // line width to the nearest half pixel value (prevents blurry lines)
-    //     ctx.lineWidth = radius/2;
-    //     ctx.strokeStyle = lineColor;
-    //     ctx.setLineDash([]);
-    //     ctx.beginPath();
+    function quadraticBezier(x1, y1, x2, y2) {
+        
+        console.log(`x1: ${x1} y1: ${y1} x2: ${x2} y2: ${y2}`);
 
-    //     var y = NaN;
-    //     const n = xUnits.length;
-    //     let x = startX;
-    //     const xIncrement = floorPixel((width - startX - p) / (n-1));
+        // midpoint coords
+        sx = Math.abs(x2 + x1) / 2;
+        sy = Math.abs(y2 + y1) / 2;
 
-    //     for (let i=0; i < n; i++) {
-    //         // calculate relative pos on vertical axis
-    //         let relativePos = graphValues[i] / upperBound;
-    //         let oldY = y;
-    //         y = startY + relativePos * (endY - startY);
+        console.log(`sx: ${sx} sy: ${sy}`);
 
-    //         // previous line end (bezier curve)
-    //         // ctx.bezierCurveTo(calculateCubicBezier(x - xIncrement, oldY, x, y));
-    //         ctx.lineTo(x, height - y);
-    //         ctx.stroke();
+        return [sx, sy, x2, y2];
+    }
 
-    //         // fill space under line
-    //         ctx.globalAlpha = 0.15;
-    //         ctx.lineTo(x, height - startY);
-    //         ctx.lineTo(x - xIncrement, height - startY);
-    //         ctx.fill();
-    //         ctx.globalAlpha = 1;
-
-    //         // legend text
-    //         ctx.beginPath();
-    //         ctx.fillStyle = legendColor;
-    //         ctx.fillText(xUnits[i], x, height - p + floorPixel(p));
-    //         // data intersection dot
-    //         ctx.fillStyle = lineColor;
-    //         ctx.arc(x, height - y, radius, 0, Math.PI * 2);
-    //         ctx.fill();
-
-    //         // next line beggining
-    //         ctx.beginPath();
-    //         ctx.moveTo(x, height - y);
-
-    //         x += xIncrement;
-    //     }
-    // }
-     
     function renderGraph(width, height, ratio) {
         const ctx = canvas.getContext("2d");
 
@@ -265,48 +195,69 @@
         // ==========================================================================
         ctx.textBaseline = "ideographic";
         ctx.textAlign = "center";
-        ctx.lineWidth = mainLineWidth;
-        ctx.strokeStyle = lineColor;
-        ctx.setLineDash([]);
-        ctx.beginPath();
 
-        y = NaN;
         let x = textWidth + g;
         const xIncrement = floorPixel((width - x - p) / (units["x"].length-1));
+        // used for later rendering of graph line
+        var points = [];
 
         for (let i=0; i < units["x"].length; i++) {
-            // calculate relative pos on vertical axis
-            let relativePos = values[i] / upperBound;
-            let oldY = y;
-            y = startY + relativePos * (endY - startY);
-
-            // previous line end (bezier curve)
-            ctx.bezierCurveTo(...calculateCubicBezier(x - xIncrement, oldY, x, height - y));
-            // ctx.lineTo(x, height - y);
-            ctx.stroke();
+            // calculate relative pos for later use when drawing graph main lines
+            let pos = {};
+            pos.x = x;
+            pos.y = height - (startY + (values[i] / upperBound) * (endY - startY));
+            
+            points.push(pos);
 
             // fill space under line
-            ctx.globalAlpha = 0.15;
-            ctx.lineTo(x, height - startY);
-            ctx.lineTo(x - xIncrement, height - startY);
-            ctx.fill();
-            ctx.globalAlpha = 1;
+            // ctx.globalAlpha = 0.15;
+            // ctx.lineTo(x, height - startY);
+            // ctx.lineTo(x - xIncrement, height - startY);
+            // ctx.fill();
+            // ctx.globalAlpha = 1;
 
             // legend text
             ctx.beginPath();
             ctx.fillStyle = legendColor;
             ctx.fillText(units["x"][i], x, height - p + floorPixel(p));
             // data intersection dot
-            ctx.fillStyle = lineColor;
-            ctx.arc(x, height - y, radius, 0, Math.PI * 2);
-            ctx.fill();
-
-            // next line beggining
-            ctx.beginPath();
-            ctx.moveTo(x, height - y);
+            // ctx.fillStyle = lineColor;
+            // ctx.arc(x, height - y, radius, 0, Math.PI * 2);
+            // ctx.fill();
 
             x += xIncrement;
         }
+        // Main graph line
+        // ==========================================================================
+        ctx.strokeStyle = lineColor;
+        ctx.fillStyle = lineColor;
+        ctx.lineWidth = mainLineWidth
+        ctx.setLineDash([]);
+        ctx.beginPath();
+
+        // move to the first point
+        ctx.moveTo(points[0].x, points[0].y);
+
+        for (var i=1; i < points.length - 2; i++) {
+            var cpx = (points[i].x + points[i+1].x) / 2;
+            var cpy = (points[i].y + points[i+1].y) / 2;
+            ctx.quadraticCurveTo(points[i].x, points[i].y, cpx, cpy);
+            ctx.stroke();
+
+            // dot
+            ctx.beginPath();
+            ctx.arc(points[i].x, points[i].y, radius, 0, Math.PI * 2);
+            ctx.fill();
+            ctx.beginPath();
+
+            // ctx.moveTo(points[i].x, points[i].y);
+        }
+        
+        ctx.quadraticCurveTo(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
+        ctx.stroke();
+
+
+
     }
 
     function updateCanvasSize() {
